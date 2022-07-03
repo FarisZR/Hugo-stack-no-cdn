0dcf4a7685ba8cfc63e0435a5d7a2183e14a762e
FarisZR <35614734+fariszr@users.noreply.github.com>
2022-07-04 02:59:57 +0300

    Create Hash check file, which checks hash diff between remote and local

---
+++ .automation/hash-check.sh
@@ -0,0 +1,15 @@
+#/bin/sh
+
+# 1=remote file name 2=local-file 3=js file name 4=which row 5=var-name
+#Get value from the local file and remote file, and compare 
+
+remote=$(cat $1 | shyaml get-value $3.$4.integrity)
+local=$(cat $2 | shyaml get-value $3.$4.integrity)
+
+# if the hashes match, then it means the file is up-to-date, so no need to update.
+if [ "$remote" == "$local" ] 
+then
+    echo "::set-output name=$5::false"
+else
+    echo "::set-output name=$5::true"
+fi
