97c0c736bb5535148acfabce16bee5fa3deda0df
FarisZR <35614734+fariszr@users.noreply.github.com>
2022-07-04 17:09:49 +0300

    Add ability to compare URLs

--- .automation/hash-check.sh
+++ .automation/hash-check.sh
@@ -1,15 +1,15 @@
 #/bin/sh
 
-# 1=remote file name 2=local-file 3=js file name 4=which row 5=var-name
+# 1=remote file name 2=local-file 3=js file name 4=which row 5=which thing to compare 6=var-name
 #Get value from the local file and remote file, and compare 
 
-remote=$(cat $1 | shyaml get-value $3.$4.integrity)
-local=$(cat $2 | shyaml get-value $3.$4.integrity)
+remote=$(cat $1 | shyaml get-value $3.$4.$5)
+local=$(cat $2 | shyaml get-value $3.$4.$5)
 
 # if the hashes match, then it means the file is up-to-date, so no need to update.
 if [ "$remote" == "$local" ] 
 then
-    echo "::set-output name=$5::false"
+    echo "::set-output name=$6::false"
 else
-    echo "::set-output name=$5::true"
+    echo "::set-output name=$6::true"
 fi
